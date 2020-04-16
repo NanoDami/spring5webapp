f7ce11cc40234743d1ea084d171792741c31b83a
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-16 12:49:33 +0200

    Added a Publisher entity to the model.

---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,106 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String adressLine1;
+    private String adressLine2;
+    private String postalCode;
+    private String city;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String adressLine1, String adressLine2, String postalCode, String city) {
+        this.name = name;
+        this.adressLine1 = adressLine1;
+        this.adressLine2 = adressLine2;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", adressLine1='" + adressLine1 + '\'' +
+                ", adressLine2='" + adressLine2 + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdressLine1() {
+        return adressLine1;
+    }
+
+    public void setAdressLine1(String adressLine1) {
+        this.adressLine1 = adressLine1;
+    }
+
+    public String getAdressLine2() {
+        return adressLine2;
+    }
+
+    public void setAdressLine2(String adressLine2) {
+        this.adressLine2 = adressLine2;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
